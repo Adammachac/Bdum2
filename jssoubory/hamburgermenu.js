f20185bf50f88c4b56bcaa4331810f174e1fe2c9
Adammachac <148778367+Adammachac@users.noreply.github.com>
2024-12-14 14:55:28 +0100

    Add files via upload

---
+++ jssoubory/hamburgermenu.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navbarToggle = document.getElementById("navbar-toggle");
+    const navbarMenu = document.querySelector(".navbar-menu");
+
+    navbarToggle.addEventListener("click", function () {
+        navbarMenu.classList.toggle("show");
+    });
+});
